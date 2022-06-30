a0982b741cfcb14f71a57bb906f8617c2eecb2f7
sanjaiiv04 <sanjaii.vijayakumar2021@vitstudent.ac.in>
2022-06-30 16:31:24 +0530

    changes reflected

--- CONCEPTS/pointers/ptr_to_func.c
+++ CONCEPTS/pointers/ptr_to_func.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-int sum(int,int);
+void sum(int,int);
 int main()
 {
-    int res,a=10,b=20;
-    int (*ptr)(int,int);/*return type of function is int.
+    int a=10,b=20;
+    void (*ptr)(int,int);/*return type of function is int.
     so pointer is defined as int. Function has two int arguments so
     the pointer is defined wth two int arguments.*/
     ptr = &sum;
-    res = (*ptr)(a,b);/*actual parameter is given to the pointer 
+    /*res = (*ptr)(a,b);actual parameter is given to the pointer 
     which is pointing to the function*/
-    printf("%d",res);
+    (*ptr)(a,b);
 
 }
-int sum(int a,int b)
+void sum(int a,int b)
 {
-    return a+b;
+    printf("%d",a+b);
 }
